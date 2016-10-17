49c2f05456b1ea34034e7158274b56a56af78b55
Sheena Gygax <sheena.gygax@g5searchmarketing.com>
2016-10-17 15:31:00 -0700

    initial commit

---
+++ template-parts/index_mockup.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+      <h1>Design Where You Are</h1>
+      <section>
+      <h2>About</h2>
+      <p>Social impact; cross-agency coordination economic development Bill and Melinda Gates UNHCR board of directors sharing economy. Facilitate long-term revitalize, human.</p>
+      </section>
+
+      <section>
+      <h2>Philosophy</h2>
+      <p>Social impact; cross-agency coordination economic development Bill and Melinda Gates UNHCR board of directors sharing economy. Facilitate long-term revitalize, human.</p>
+      </section>
+
+      <section>
+      <h2>Something Else</h2>
+      <p>Informal economies, human potential gender rights outcomes institutions social movement pathway to a better life. Freedom social analysis; results, diversity health. Policymakers affiliate accessibility Kickstarter collaborative consumption our ambitions peaceful.</p>
+      <p>&nbsp;</p>
+      </section>
+
+    </main><!-- .site-main -->
+
+    <section class="contact">
+        <h2>Get in Touch</h2>
+        <button class="ripple">Contact</button>
+    </section>
+  </div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
+
+<?php
+// if (function_exists('show_all_pages_on_index')) {
+//     echo "yes.<br />\n";
+// } else {
+//     echo "no.<br />\n";
+// }
+?>
+
+
+<?php get_footer(); ?>
