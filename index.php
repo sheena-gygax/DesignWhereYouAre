c95adff818fdc3373558db643d0dc408e386edf5
Jason Halsey <jasonhalsey@01260-MBP15R.local>
2016-10-17 20:20:12 -0700

    Custom Loop by Cat ID

--- index.php
+++ index.php
@@ -1,40 +1,47 @@
 <?php
 /**
- * @package _s
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
  */
 
 get_header(); ?>
 
-<div class="container">
-  
-
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
-   <?php 
-    $args = array( 
-      // Sheena set the category ID Here
-     'cat' => '2'
-    );
-    $the_query = new WP_Query( $args );
-   ?>
+      <?php 
+        $args = array( 
+          // Sheena set the category ID Here
+         'cat' => '2'
+        );
+        $the_query = new WP_Query( $args );
+      ?>
 
-  <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+      <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-  <h1><?php the_title() ;?></h1>      
-  <?php the_excerpt(); ?>
+      <h1><?php the_title() ;?></h1>      
+      <?php the_content(); ?>
 
-<?php endwhile; else: ?>
+      <?php endwhile; else: ?>
 
-  <p>Sorry, there are no posts to display</p>
+        <p>Sorry, there are no posts to display</p>
 
-<?php endif; ?>
+      <?php endif; ?>
 
-<?php wp_reset_query(); ?>
-    </main><!-- #main -->
-  </div><!-- #primary -->
+      <?php wp_reset_query(); ?>
 
-  <?php get_sidebar(); ?>
+    </main><!-- .site-main -->
+  </div><!-- .content-area -->
 
-</div><!-- #container -->
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
